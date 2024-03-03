4346310ebb13d26147fb2ca578a8b0b1a299352a
cpu170 <125718856+cpu170@users.noreply.github.com>
2024-03-03 20:59:00 +0800

    Update 量子资源.js

--- n/g/js/量子资源.js
+++ n/g/js/量子资源.js
@@ -13,8 +13,8 @@ var rule = {
     timeout: 5000,
     limit: 6,
     url: '/api.php/provide/vod?ac=detail&t=fyclass&pg=fypage&f=',
-    class_name: '国产剧&香港剧&韩国剧&动作片&剧情片&喜剧片&爱情片&国产动漫',
-    class_url: '13&14&15&6&11&7&8&29',
+    class_name: '国产剧&伦理剧&香港剧&韩国剧&动作片&剧情片&喜剧片&爱情片&国产动漫',
+    class_url: '13&14&15&6&11&7&8&29&34',
     推荐: 'json:list;vod_name;vod_pic;vod_remarks;vod_id', // double: true, // 推荐内容是否双层定位
     // 一级: 'json:list;vod_name;vod_pic;vod_remarks;vod_id',
     一级: `js:
@@ -30,7 +30,7 @@ var rule = {
         }
         let d = [];
         // 忽略分类
-        let cate_exclude = '34,35,45';
+        let cate_exclude = '35,45';
         let type_id = getParam(input,'t');
         if(!cate_exclude.match(type_id)){
             let html = request(input);
