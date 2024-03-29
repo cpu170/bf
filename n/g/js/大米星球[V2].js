8b512d80dc3d4716fe7184fffd2792451f306ff3
cpu170 <125718856+cpu170@users.noreply.github.com>
2024-03-29 22:39:53 +0800

    Update 大米星球[V2].js

--- n/g/js/大米星球[V2].js
+++ n/g/js/大米星球[V2].js
@@ -16,7 +16,7 @@ var rule = {
 	detailUrl:'/detail?vod_id=fyid',
 	searchUrl: '/search?text=**&pg=fypage',
 	searchable: 2,
-	quickSearch: 0,
+	quickSearch: 1,
 	filterable:0,//是否启用分类筛选,
 	// headers:{'User-Agent':'Dart/2.14 (dart:io)'},
 	headers:{'User-Agent':'okhttp/4.1.0'},
