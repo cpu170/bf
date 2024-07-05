122e5c74fa395ced5f61484f958fc786a682b89f
cpu170 <125718856+cpu170@users.noreply.github.com>
2024-07-05 19:04:19 +0800

    Update 新视觉影视.js

--- n/g/js/新视觉影视.js
+++ n/g/js/新视觉影视.js
@@ -7,7 +7,7 @@ var rule={
 	title:'新视觉影视',
 	模板:'mxone5',
 	// host:'https://www.6080dy4.com',
-	host:'https://www.5280.fun',
+	host:'https://www.6080yy3.com/',
 	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":PC_UA}});let src=jsp.pdfh(html,".go:eq(0)&&a&&href");print(src);HOST=src',
 	// url:'/vodshow/fyclass--------fypage---.html',
 	url:'/vodshow/fyfilter.html',
