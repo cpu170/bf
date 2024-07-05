de58f28124a01a176127491409c188910efca885
cpu170 <125718856+cpu170@users.noreply.github.com>
2024-07-05 16:41:04 +0800

    Update nongmin.js

--- n/g/lib/nongmin.js
+++ n/g/lib/nongmin.js
@@ -5,7 +5,7 @@ var rule={
 	//host:'https://www.nmddd.com',
 	//host:'https://v.xiangdao.me',
 	// host:'https://v.emsdn.cn',
-	host:'https://www.nmdvd.com/',
+	host:'https://www.wwgz.cn/',
 	hostJs:'print(HOST);let html=request(HOST,{headers:{"User-Agent":MOBILE_UA}});let src = jsp.pdfh(html,"body&&a:eq(1)&&href");print(src);HOST=src',
 	url:'/vod-list-id-fyfilter.html',
 	filterable:1,//是否启用分类筛选,
